21debc6bb8018775139ebd6c548a69f911c21839
Arjun Saluja <arjunsaluja2004@gmail.com>
2024-10-16 23:02:50 +0530

    javascript

---
+++ _dom/dom.js
@@ -0,0 +1,15 @@
+//DOCUMENT OBJECT MODEL
+
+// getelementbytag
+var a=document.getElementsByTagName('li');// div bhi askta ha
+console.log(a);
+//getelemenbyclass name
+var a=document.getElementsByClassName('hero');
+console.log(a);
+//getelemntbyid
+var a=document.getElementById('ul');
+console.log(a);
+//queryselector
+var a=document.querySelector('.hero,#id');// div bhi   ha
+console.log(a);
+//queryslectorall
