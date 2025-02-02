78e22ff6d37938e5117fb875fe530969baefc21c
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-02-02 23:40:46 +0530

    js

---
+++ map.js
@@ -0,0 +1,19 @@
+// maps
+
+var map1= new Map([[1,"one",],[
+    "fname","mickey"],["whole no",[1,2,3,4,5]]])
+
+    console.log(map1.get('fname'));
+
+var k= map1.keys();
+ for( var key of k){
+    console.log(key);
+ }
+
+    console.log(map1);
+// 2 method
+
+    // var map2 =new Map();
+
+    // map2.set("fname","mouse")
+    // console.log(map2);
