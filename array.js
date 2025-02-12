f88d39805c872fb51d5a9a684a4c536777aa44b3
Arjun Saluja <arjunsaluja2004@gmail.com>
2025-02-12 19:33:46 +0530

    js

---
+++ array.js
@@ -0,0 +1,14 @@
+let arr1=["as","chad", "neo","magnaa"]
+// let item1=arr1[0];
+// let item2 =arr1[1];
+
+// console.log(item1);
+// console.log(item2);
+
+
+let[item1, item2, ...item3]=arr1;
+
+console.log(item1);
+console.log(item2);
+console.log(item3);
+//destruct array
